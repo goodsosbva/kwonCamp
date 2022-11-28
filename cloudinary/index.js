5933016e5c5da06b3af6dfc8b9823872d75e899b
kwon <goodsosbva@naver.com>
2022-11-28 15:13:46 +0900

    kcamp code

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'YelpCamp',
+        folder: 'kwonCamp',
         allowedFormats: ['jpeg', 'jpg', 'png']
     }
 })
